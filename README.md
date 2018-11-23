ff499dabfd1aff78ce03cddda6f2c0557131a14c
vishal3152 <vishal3152@gmail.com>
2018-11-23 12:35:07 +0200

    comments  added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RegisterUser
 Fabric-ca-utility to register user
+
+Modify registerUser.js.
+Run node registerUser.js
--- registerUser.js
+++ registerUser.js
@@ -2,45 +2,51 @@ var hfc = require('fabric-client');
 
 async function getFabricClient() {
 
-    let client = hfc.loadFromConfig('./org1-config.json');
-
+    // Load the network configuration file
+    let client = hfc.loadFromConfig('./org3-config.json');
+    //Init credential store, make sure that credential store path is defined in the network config file
     await client.initCredentialStores();
-    var ca = client.getCertificateAuthority();
-    //await client.setUserContext(ca.getRegistrar())
 
-
+    // Get admin user context, this would act as a registrar while enrolling other users.
     await client.setUserContext({
         username: "admin",
-        password: "c2556b51fc"
+        password: "??????"
     });
 
     return client;
 }
-
-
 
 var registerUser = async function () {
     const client = await getFabricClient();
 
     var fabric_ca_client = client.getCertificateAuthority();
 
+    // describe identity, with attributes if you wish to implement attribute based access control in your chaincode
     var registerReq = {
-        enrollmentID: "test",
+        enrollmentID: "test_user",
         role: "user",
         maxEnrollments : 12,
-        affiliation : "org1",
+        affiliation : "org3",
         attrs: [{
-            name: 'bic',
-            value: 'ARABJO',
+            name: 'attr1',
+            value: 'test',
+            ecert: true
+        },
+        {
+            name: 'attr2',
+            value: 'test2',
             ecert: true
         }]
     }
+    //get registrar
 var registrar=await client.getUserContext();
+//Regsiter User
 var enrolSecret=await fabric_ca_client.register(registerReq,registrar)
-    console.log(enrolSecret);
+// print enroll secret
+console.log(enrolSecret);
 }
 
-
+/*
 var revokeUser = async function () {
     const client = await getFabricClient();
 
@@ -53,7 +59,7 @@ var revokeUser = async function () {
 var registrar=await client.getUserContext();
 var resp=await fabric_ca_client.revoke(revokeRequest,registrar)
     console.log(resp);
-}
+}*/
 
 registerUser().then(() => {});
 //revokeUser().then(() => {});
